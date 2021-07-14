ccefda12bb1523a955dcf62ba131222d82b44cb5
Steve <svenoaks@gmail.com>
2021-07-14 18:33:25 -0400

    Initial Commit

---
+++ testPopupUI/testPopupUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testPopupUIApp.swift
+//  testPopupUI
+//
+//  Created by Steve Myers on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct testPopupUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
